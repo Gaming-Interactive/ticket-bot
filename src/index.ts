634d11fc9f946d80cc3062501d17da87f03bdbae
Devil <59297151+devil123w@users.noreply.github.com>
2023-02-05 17:19:58 +0100

    node change

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import * as DC from "discord.js"
+const client = new DC.Client({ intents: [DC.GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+
+// add enviroment var here
+client.login("");
